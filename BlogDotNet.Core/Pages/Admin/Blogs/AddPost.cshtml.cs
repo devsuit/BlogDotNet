7b59ca3e57b7c1ef2223da03774b243b8c178e94
Syed Qalb-e-Abbas <sqabbas1994@gmail.com>
2025-03-12 13:45:09 +0500

    Add Post Page, Front End Form Design completed.

---
+++ BlogDotNet.Core/Pages/Admin/Blogs/AddPost.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogDotNet.Core.Pages.Admin.Blogs
+{
+    public class AddPostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

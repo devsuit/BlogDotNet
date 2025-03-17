5b0aa40e46cdecabea08df161b7451f06bd25a25
Syed Qalb-e-Abbas <sqabbas1994@gmail.com>
2025-03-17 13:35:33 +0500

    update edit page

--- BlogDotNet.Core/Pages/Admin/Blogs/Edit.cshtml.cs
+++ BlogDotNet.Core/Pages/Admin/Blogs/Edit.cshtml.cs
@@ -11,17 +11,44 @@ namespace BlogDotNet.Core.Pages.Admin.Blogs
 
         [BindProperty]
         public BlogPost BlogPost { get; set; }
+
         public EditModel(BlogDbContext blogDbContext)
         {
             this.blogDbContext = blogDbContext;
         }
-        public void OnGet(Guid id)
-        { var blogPost = blogDbContext.BlogPosts.Find(id);
-            //if (blogPost == null)
-            //{
-            //    RedirectToPage("/Admin/Blogs/List");
-            //}
-            //BlogPost = blogPost;
+
+        public IActionResult OnGet(Guid id)
+        {
+            BlogPost = blogDbContext.BlogPosts.Find(id);
+
+            if (BlogPost == null)
+            {
+                return RedirectToPage("/Admin/Blogs/List"); // Redirect if blog post not found
+            }
+
+            return Page(); // Return the page with BlogPost data
+        }
+
+        public IActionResult OnPost()
+        {
+            var existingBlogPost = blogDbContext.BlogPosts.Find(BlogPost.Id);
+
+            if (existingBlogPost != null)
+            {
+                existingBlogPost.Heading = BlogPost.Heading;
+                existingBlogPost.PageTitle = BlogPost.PageTitle;
+                existingBlogPost.Content = BlogPost.Content;
+                existingBlogPost.ShortDescription = BlogPost.ShortDescription;
+                existingBlogPost.FeaturedImageUrl = BlogPost.FeaturedImageUrl;
+                existingBlogPost.UrlHandle = BlogPost.UrlHandle;
+                existingBlogPost.PablishedDate = BlogPost.PablishedDate; // Fixed Typo
+                existingBlogPost.Author = BlogPost.Author;
+                existingBlogPost.Visible = BlogPost.Visible;
+
+                blogDbContext.SaveChanges();
+            }
+
+            return RedirectToPage("/Admin/Blogs/List");
         }
     }
 }
